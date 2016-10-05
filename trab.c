f8cc87fe6a21e9f5325a645a6cf33ab3b1e20401
Evelim Carla Ribeiro <ecr10@inf.ufpr.br>
2016-10-04 21:09:45 -0300

    Incluido alguns outros simbolos para tirar do dicionario.txt
    Signed-off-by: Evelim Carla Ribeiro <ecr10@inf.ufpr.br>

--- trab.c
+++ trab.c
@@ -6,7 +6,9 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define SEPARATORS " ,;.-\":%?!()"
+
+#define SEPARATORS " ,;.\":?!()"
+
 
 int isStringInBuffer(char *str, char **buffer, int size){
     int i;
